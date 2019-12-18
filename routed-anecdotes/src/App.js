aecbe33c09c6e3e20abb44bc84436808bc24d4f2
Edgar Cheverier <edgar.aguilar@devex.com>
2019-12-18 17:09:47 +0100

    Part7 7.1: routed anecdotes, step1

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link, Redirect, withRouter } from 'react-router-dom';
 
 const Menu = () => {
   const padding = {
@@ -6,9 +7,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to='/'> anecdotes </Link>
+      <Link style={padding} to='/create'> create new </Link>
+      <Link style={padding} to='/about'> about </Link>
     </div>
   )
 }
@@ -124,12 +125,16 @@ const App = () => {
 
   return (
     <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
+      <Router>
+        <div>
+          <h1>Software anecdotes</h1>
+          <Menu />
+          <Route exact path='/' render={() => <AnecdoteList anecdotes={anecdotes} /> }/>
+          <Route path='/create' render={() => <CreateNew addNew={addNew} />} />
+          <Route path='/about' render={() => <About />} />
+          <Footer />
+        </div>
+      </Router>
     </div>
   )
 }
